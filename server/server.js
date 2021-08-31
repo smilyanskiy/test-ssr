d2887405c76244892ee8fc17e5f054e7e332c07c
Smilyanskiy <v.smilyanskiy@betinvest.com>
2021-08-31 17:03:46 +0300

    add node server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+
+import App from "../src/App";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
